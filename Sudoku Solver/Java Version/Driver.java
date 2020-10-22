4a82032218b8d0fc5397801acb18b6f0c8d59b69
Seth Santos <sethsantos789@gmail.com>
2020-10-22 02:22:59 -0400

    Initializes Driver class

---
+++ Sudoku Solver/Java Version/Driver.java
@@ -0,0 +1,11 @@
+/* Sudoku Solver
+ * Implemented via backtracking algorithm
+ * 9x9 grid
+ *
+ */
+
+public class Driver {
+    public static void main(String[] args){
+        new Solver().run();
+    }
+}
